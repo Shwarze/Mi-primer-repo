6f9e6c8fd0e855919aaa922d38911b5b698253e2
Shwarze <Shwarzegz@gmail.com>
2022-08-13 20:32:08 -0500

    Primer Repo

---
+++ Javita/app.js
@@ -0,0 +1,11 @@
+function ClikRs (){
+    var AdRs = document.getElementsByClassName ('RedSocial');
+    for (var i = 0; i < AdRs.length; i++){
+        AdRs[i].classList .toggle('Notoy1');
+    }
+}
+function ClikMenu (){
+    var AdMn = document.getElementsByClassName ('Barrita');
+    for (var i = 0; i < AdMn.length; i++){
+        AdMn[i].classList .toggle('Notoy2');}
+}
